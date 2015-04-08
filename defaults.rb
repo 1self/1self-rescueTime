1a3c0cc0ebea4f9d9ba8c8e165cb39307cf373df
Devaroop <devaroop123@yahoo.co.in>
2015-04-08 16:52:55 +0530

    change api hosts

--- defaults.rb
+++ defaults.rb
@@ -8,14 +8,14 @@ module Defaults
   end
 
   SESSION_SECRET = from_file("session_secret.txt")
-  HOST_URL = "https://oneself-rescuetime.herokuapp.com"
+  HOST_URL = "https://rescuetime.1self.co"
   SYNC_ENDPOINT = "/sync"
 
   RESCUE_TIME_CLIENT_ID = from_file("rescuetime_client_id.txt")
   RESCUE_TIME_CLIENT_SECRET = from_file("rescuetime_client_secret.txt")
-  RESCUE_TIME_CALLBACK_URL = 'https://rescuetime.1self.co/oauthredirect'
+  RESCUE_TIME_CALLBACK_URL = HOST_URL + '/oauthredirect'
 
-  ONESELF_API_HOST = "http://api.1self.co"
+  ONESELF_API_HOST = "http://api-staging.1self.co"
   ONESELF_STREAM_REGISTER_ENDPOINT = "/v1/users/%s/streams"
   ONESELF_SEND_EVENTS_ENDPOINT = "/v1/streams/%s/events/batch"
   ONESELF_APP_ID = from_file("oneself_app_id.txt")
